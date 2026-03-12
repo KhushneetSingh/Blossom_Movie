81c2a1c6ec5f9944d843f0065dcd22a77e719c73
Khushneet Singh <singhkhushneet601@gmail.com>
2026-03-12 22:33:05 +0530

    Initial Commit

---
+++ Blossom Movie/Blossom_MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blossom_MovieApp.swift
+//  Blossom Movie
+//
+//  Created by Khushneet Singh on 12/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Blossom_MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
