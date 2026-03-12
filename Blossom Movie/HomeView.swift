824ffaaa78efe12c221476c336df6d8b251ff9e2
Khushneet Singh <singhkhushneet601@gmail.com>
2026-03-13 03:50:00 +0530

    Adding the Style to a button and adding new assets to use

--- Blossom Movie/HomeView.swift
+++ Blossom Movie/HomeView.swift
@@ -26,11 +26,17 @@ struct HomeView: View {
                 } label: {
                     Text(Constants.playString)
                         .frame(width: 100, height: 50)
+                        .foregroundStyle(.buttonText)
                         .bold()
                         .background {
                             RoundedRectangle(cornerRadius: 20, style: .continuous)
-                                .stroke(lineWidth: 5)
+                                .stroke(.buttonBorder,lineWidth: 5)
                         }
+                }
+                Button {
+                    
+                } label: {
+                    Text(Constants.downloadString)
                 }
             }
         }
